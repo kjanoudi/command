0edd58c730750a8166badd998a4ffd2191a77dd9
mico <m@psytron.com>
2018-03-25 12:23:08 -0700

    add python version

--- detect.sh
+++ detect.sh
@@ -1,8 +1,19 @@
 echo "Detect Environment"
 
-echo "You are on a "$(getconf LONG_BIT)"-bit architecture"
+echo " "$(getconf LONG_BIT)"-bit architecture"
 
 # get conditional check if kubernetes live:
 # run and install 
 # http://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/darwin/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
+
+
+
+# run minikube:
+# minikube start
+
+# 
+python --version
+
+# after it starts, it detects its own repo, git clones and unzips more stuff
+# 
