5ec98c06e6f92a65692ec3ce6190a5028467dea0
mico <m@psytron.com>
2018-03-25 12:14:42 -0700

    add command

--- detect.sh
+++ detect.sh
@@ -1,2 +1,8 @@
 echo "Detect Environment"
+
 echo "You are on a "$(getconf LONG_BIT)"-bit architecture"
+
+# get conditional check if kubernetes live:
+# run and install 
+# http://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/darwin/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
