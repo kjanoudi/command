15a8193f77855eb3632d519f3e8f655211f99bc7
psytron <m@psytron.com>
2018-03-25 16:00:30 -0700

    Update gitio.sh
    update shortnener example

--- gitio.sh
+++ gitio.sh
@@ -1,20 +1,10 @@
-Git.io: GitHub URL Shortener
- Nov 10, 2011
-technoweenie
-technoweenie
- Broadcast
-Do you have a GitHub URL you’d like to shorten? Use Git.io!
 
-$ curl -i https://git.io -F "url=https://github.com/..."
-HTTP/1.1 201 Created
-Location: https://git.io/abc123
 
-$ curl -i https://git.io/abc123
-HTTP/1.1 302 Found
-Location: https://github.com/...
-You can specify your own code to setup your own vanity URL:
+# Git.io: GitHub URL Shortener
 
-$ curl -i https://git.io -F "url=https://github.com/technoweenie" \
-    -F "code=t"
-HTTP/1.1 201 Created
-Location: https://git.io/t
+# Curl to shortener with ( -F ) Form POST prams: 
+curl -i https://git.io \
+    -F "url=https://raw.githubusercontent.com/psytron/command/master/detect.sh" \
+    -F "code=yoyo"
+
+#Location: https://git.io/yoyo
